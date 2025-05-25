dfd51e42333c819e32831a56fc83544ba4879a32
NebeProgramer <87898320+NebeProgramer@users.noreply.github.com>
2025-05-25 14:39:52 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1156,10 +1156,6 @@ app.delete('/api/redes-sociales/:id', async (req, res) => {
     }
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
 app.listen(port, () => {
     console.log(`Servidor escuchando en http://0.0.0.0:${port}`);
 });
