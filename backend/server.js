d88699b54942515d5220e71f9c93fca8552b5baf
NebeProgramer <87898320+NebeProgramer@users.noreply.github.com>
2025-05-27 08:58:57 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ mongoose.connection.on('error', (err) => {
 });
 
 // Middleware
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(bodyParser.json({ limit: '5mb' }));
 app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
