7c3498c3ddc58fa90bee21b2601ab3267080a316
NebeProgramer <87898320+NebeProgramer@users.noreply.github.com>
2025-06-15 23:52:12 -0500

    Update scriptAdmin.js

--- backend/public/js/scriptAdmin.js
+++ backend/public/js/scriptAdmin.js
@@ -821,7 +821,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Manejar el botón para agregar red social
     document.getElementById('btn-agregar-red').addEventListener('click', async () => {
-        const contenedor = document.querySelector('.redes_sociales');
+        const contenedor = document.querySelector('.redes-sociales');
         contenedor.innerHTML = ''; // Limpiar el contenedor para preparar la edición
         contenedor.style.flexDirection = 'column'; // Cambia a columna en modo edición
 
@@ -896,7 +896,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     document.getElementById('btn-guardar-red').addEventListener('click', async () => {
-        const contenedor = document.querySelector('.redes_sociales');
+        const contenedor = document.querySelector('.redes-sociales');
         contenedor.style.flexDirection = 'row'; // Vuelve a fila al guardar
         const items = contenedor.querySelectorAll('li');
 
@@ -971,7 +971,7 @@ document.addEventListener('DOMContentLoaded', () => {
         cargarRedesSociales(); // Recargar las redes sociales
         document.getElementById('btn-guardar-red').style.display = 'none'; // Ocultar el botón de guardar
         document.getElementById('Cancelar').style.display = 'none';
-        const contenedor = document.querySelector('.redes_sociales');
+        const contenedor = document.querySelector('.redes-sociales');
         contenedor.style.flexDirection = 'row'; // Vuelve a fila al cancelar
     });
 
