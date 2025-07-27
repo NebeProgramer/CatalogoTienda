7707d80d01bb7ea471ca910462f934869fc0fb3f
NebeProgramer <87898320+NebeProgramer@users.noreply.github.com>
2025-07-26 23:56:39 -0500

    ..

--- backend/public/js/script.js
+++ backend/public/js/script.js
@@ -299,13 +299,13 @@ document.addEventListener('DOMContentLoaded', () => {
         if (usuarioSesion && usuarioSesion.nombre) {
             const bienvenida = document.querySelector('.bienvenida');
             if (bienvenida) {
-                bienvenida.textContent = usuarioSesion.nombre ? `Bienvenido ${usuarioSesion.nombre} a nuestra tienda!` : `Bienvenido Invitado a nuestra tienda!`;
+                bienvenida.textContent = usuarioSesion.nombre ? `¡Bienvenido ${usuarioSesion.nombre} a PawMarket!` : `¡Bienvenido Invitado a PawMarket!`;
             } else {
 
                 // Crear y mostrar el mensaje de bienvenida
                 const bienvenida = document.createElement('h3');
                 bienvenida.classList.add('bienvenida');
-                bienvenida.textContent = usuarioSesion.nombre ? `Bienvenido ${usuarioSesion.nombre} a nuestra tienda!` : `Bienvenido Invitado a nuestra tienda!`;
+                bienvenida.textContent = usuarioSesion.nombre ? `¡Bienvenido ${usuarioSesion.nombre} a PawMarket!` : `¡Bienvenido Invitado a PawMarket!`;
                 bienvenida.style.textAlign = 'center';
                 bienvenida.style.marginBottom = '20px';
                 crudSection.insertAdjacentElement('beforebegin', bienvenida);
